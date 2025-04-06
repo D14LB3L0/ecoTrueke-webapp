3ac6a1dc00b41e8b96396d9dd6514400e871ad1e
D14LB3L0 <diegobdev2024@gmail.com>
2025-04-06 17:33:03 -0500

    added padding

--- src/modules/auth/components/AuthLayout.tsx
+++ src/modules/auth/components/AuthLayout.tsx
@@ -12,7 +12,7 @@ interface IAuthLayout {
 export const AuthLayout = ({ formComponent, image }: IAuthLayout) => {
     return (
         <div className="grid md:grid-cols-2 items-center p-10 lg:p-8 2xl:p-16 gap-40 w-full min-h-screen overflow-hidden bg-background transition-all duration-300 ease-in-out">
-            <div className="lg:max-w-[650px] md:flex md:justify-end md:ml-auto">{formComponent}</div>
+            <div className="lg:max-w-[650px] md:flex md:justify-end md:ml-auto pl-10">{formComponent}</div>
             <div className="hidden md:block max-w-[650px] overflow-hidden max-h-[600px] rounded-xl">
                 <img src={image.src} alt={image.alt} />
             </div>
