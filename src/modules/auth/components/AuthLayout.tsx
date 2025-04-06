3474af765d42b83c8abaa614633fecb43a4490a5
Diego Bermudez <Diego.Bermudez92@gmail.com>
2025-04-06 13:00:11 -0500

    added view desktop login version

--- src/modules/auth/components/AuthLayout.tsx
+++ src/modules/auth/components/AuthLayout.tsx
@@ -5,8 +5,8 @@ interface IAuthLayout {
 
 export const AuthLayout = ({ formComponent }: IAuthLayout) => {
     return (
-        <div className="grid md:grid-cols-2 items-center p-10 lg:p-20 gap-20 w-full min-h-screen overflow-hidden bg-background transition-all duration-300 ease-in-out">
-            <div className="lg:max-w-[650px] pl-0 md:pl-20 lg:pl-40">{formComponent}</div>
+        <div className="grid md:grid-cols-2 items-center p-10 lg:p-8 2xl:p-16 gap-20 w-full min-h-screen overflow-hidden bg-background transition-all duration-300 ease-in-out">
+            <div className="lg:max-w-[650px] pl-0 lg:pl-20 2xl:pl-40">{formComponent}</div>
             <div className="hidden md:block max-w-[650px]">
                 <img src="/auth/logo-vertical.webp" alt="EcoTrueke logo vertical" className="rounded-xl" />
             </div>
