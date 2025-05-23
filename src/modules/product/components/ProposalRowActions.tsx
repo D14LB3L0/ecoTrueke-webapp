8b88a6395bf6897dbdb213966e6185025894927a
D14LB3L0 <diegobdev2024@gmail.com>
2025-05-23 23:50:05 +0100

    loading actions

--- src/modules/product/components/ProposalRowActions.tsx
+++ src/modules/product/components/ProposalRowActions.tsx
@@ -1,26 +1,60 @@
+import { Spinner } from "@/components/ui/spinner";
 import { X, Check } from "lucide-react"; // Íconos opcionales
+import { useState } from "react";
 
 interface ProposalRowActions {
   proposalId: string;
 }
 
 export const ProposalRowActions = ({ proposalId }: ProposalRowActions) => {
-  const handleAcceptRequest = () => {};
-  const handleRejectRequest = () => {};
+  const [loadingAcceptRequest, setLoadingAcceptRequest] =
+    useState<boolean>(false);
+  const [loadingRejectRequest, setLoadingRejectRequest] =
+    useState<boolean>(false);
+
+  const handleAcceptRequest = () => {
+    try {
+      setLoadingAcceptRequest(true);
+    } catch {
+    } finally {
+      setLoadingAcceptRequest(false);
+    }
+  };
+  const handleRejectRequest = () => {
+    try {
+      setLoadingRejectRequest(true);
+    } catch {
+    } finally {
+      setLoadingRejectRequest(false);
+    }
+  };
 
   return (
     <div className="flex gap-4 items-center">
-      <X
-        className="mr-1 cursor-pointer  text-muted-foreground hover:text-destructive transition-colors duration-200"
-        onClick={() => handleRejectRequest}
-        size={20}
-      />
-
-      <Check
-        className="mr-1 cursor-pointer  text-muted-foreground hover:text-primary transition-colors duration-200"
-        onClick={() => handleAcceptRequest}
-        size={20}
-      />
+      {loadingRejectRequest ? (
+        <Spinner className="text-destructive" size="sm" />
+      ) : (
+        <X
+          className="mr-1 cursor-pointer  text-muted-foreground hover:text-destructive transition-colors duration-200"
+          onClick={(e) => {
+            handleRejectRequest;
+            e.stopPropagation();
+          }}
+          size={20}
+        />
+      )}
+      {loadingAcceptRequest ? (
+        <Spinner className="text-muted-foreground" size="sm" />
+      ) : (
+        <Check
+          className="mr-1 cursor-pointer  text-muted-foreground hover:text-primary transition-colors duration-200"
+          onClick={(e) => {
+            handleAcceptRequest;
+            e.stopPropagation();
+          }}
+          size={20}
+        />
+      )}
     </div>
   );
 };
